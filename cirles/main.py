e0aaba44a2635629f7c9eb376e4a017b6cbb4217
G4st3r21 <70285259+G4st3r21@users.noreply.github.com>
2020-11-10 19:45:42 +0300

    решена задача 2

---
+++ cirles/main.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.test = None
+
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.test = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.test:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_cirlce(qp)
+    
+    def draw_cirlce(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        self.radius = randint(0, self.height())
+        self.x, self.y = randint(0, self.width()), randint(0, self.height())
+        qp.drawEllipse(self.x - self.radius, self.y - self.radius, 2 * self.radius, 2 * self.radius)
+        self.test = None
+
+
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
